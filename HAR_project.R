c57e9dec14835d5a331f520cdf04939c670c51d7
gabgel <gabriele.gelsomini@gmail.com>
2015-09-24 23:57:55 +0200

    final model and predictions

--- HAR_project.R
+++ HAR_project.R
@@ -17,17 +17,13 @@ str(testing)
 #eliminate variables with many NA's >60
 RatioNA=function(x) sum(is.na(x))/length(x)
 PercNA<-apply(X = training, 2, FUN = RatioNA)
-train<-training[,-which(PercNA>0.6)]
-test<-testing[,-which(PercNA>0.6)]
+train<-training[,-which(PercNA>0.3)]
+
 
 #eliminate first 6 covariate not numeric and the last outcome variable
-numData=function(x) {
-    l<-dim(x)[2]
-    x[,-c(1:6,l)] 
-}
+numData=function(x) x[,-c(1:6)] 
 trainNum<-numData(train)
-trainY<-train$classe
-testNum<-numData(test)
+
 
 
 #pre process variables
@@ -35,29 +31,20 @@ preObj<-preProcess(trainNum, method=c("knnImpute","center","scale"))
 trainNumProc<-predict(preObj,trainNum)
 
 #check for near zero variance
-nzv<-nearZeroVar(trainNumProc,saveMetrics = T)
+l<-dim(trainNum)[2]
+nzv<-nearZeroVar(trainNum[,-l],saveMetrics = T)
 sum(nzv$nzv) #there is no variable with near zero variance
 
-testLast<-predict(preObj, testNum)
 
 #cross validation - slice the dataset in sub train and sub test
 inTrain<-createDataPartition(y = trainY,p = 0.6,list=F)
-subTrain<-trainNumProc[inTrain,]
-subTrainY<-trainY[inTrain]
-subTest<-trainNumProc[-inTrain,]
-subTestY<-trainY[-inTrain]
-
-#train model with trees
-modFit<-train(subTrainY~., method="rpart", data=subTrain)
-print(modFit$finalModel)
-plot(modFit$finalModel, uniform=T, main="Classification Tree")
-text(modFit$finalModel, use.n=T, all=T, cex=.8)
-
-confusionMatrix(predict(modFit, newdata = subTest),subTestY) #poor classification
+subTrain<-trainNum[inTrain,]
+subTest<-trainNum[-inTrain,]
 
 #train model random forest
-modFit2<-randomForest(subTrainY~., data=subTrain,mtry=dim(subTrain)[2],importance=TRUE)
-confusionMatrix(predict(modFit2, newdata = subTest),subTestY) #99% accuracy
+modFit<-randomForest(classe~., data=subTrain, preProcess=c("knnImpute","center","scale"),importance=TRUE)
+
+confusionMatrix(predict(modFit, newdata = subTest),subTest$classe) #99% accuracy
 
 #function to write files
 pml_write_files = function(x){
@@ -69,7 +56,6 @@ pml_write_files = function(x){
 }
 
 #make prediction with model
-answers=as.character(predict(modFit2,newdata=testing))
+answers=predict(modFit,newdata=testing)
 setwd(dir = "./projectAnswers/")
 pml_write_files(answers)
-
